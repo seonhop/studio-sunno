5464390ff92f385bc60201fb1ab88cbec24976e1
seonhop <78877741+seonhop@users.noreply.github.com>
2023-06-27 21:17:08 -0400

    [UI Update] Handling max screen width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<div className="w-full mx-auto max-w-6xl">
 			<Component {...pageProps} />
-		</>
+		</div>
 	);
 }
