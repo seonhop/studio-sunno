570e176d263aba14ee0d695625a2970746c233df
seonhop <78877741+seonhop@users.noreply.github.com>
2023-07-04 19:08:37 -0400

    [Code Enhancement / Accounts Logic] Improved user upsert operation and added detailed comments

--- pages/api/users/auth.ts
+++ pages/api/users/auth.ts
@@ -3,7 +3,23 @@ import withHandler from "@/pages/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log(req.body);
+	// Destructure phone and email from the request body
+	const { phone, email } = req.body;
+
+	// Create a payload object with the phone number if it exists, otherwise with the email
+	const payload = phone ? { phone: +phone } : { email };
+
+	// Attempt to update the user with the details in the payload, if the user doesn't exist, create a new one
+	const user = await client.user.upsert({
+		where: {
+			...payload,
+		},
+		create: {
+			...payload,
+		},
+		update: {},
+	});
+	console.log(user);
 	return res.status(200).end();
 }
 
