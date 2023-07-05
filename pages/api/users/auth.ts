aea35ddf9eee98ea9bef3536d77a4796b51843ac
seonhop <78877741+seonhop@users.noreply.github.com>
2023-07-05 00:19:34 -0400

    [API Enhancement / Type Addition] Enhanced user authentication and added ResponseType
    1. Enhanced the user authentication process in auth.ts by including a check for login methods and generating a random payload for the token creation.
    2. Added a new ResponseType interface in withHandler.ts to standardize response objects.
    3. Modified handler function in withHandler.ts to use the new ResponseType.
    4. Updated token creation logic in auth.ts to use the improved loginMethod and random payload.
    5. Updated the status responses in auth.ts to use the standardized response object format.

--- pages/api/users/auth.ts
+++ pages/api/users/auth.ts
@@ -1,24 +1,33 @@
 import client from "@/pages/libs/server/client";
-import withHandler from "@/pages/libs/server/withHandler";
+import withHandler, { ResponseType } from "@/pages/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<ResponseType>
+) {
 	// Destructure phone and email from the request body
 	const { phone, email } = req.body;
 
 	// Create a payload object with the phone number if it exists, otherwise with the email
-	const payload = phone ? { phone: +phone } : { email };
+	const loginMethod = phone ? { phone: +phone } : email ? { email } : null;
+	if (!loginMethod) return res.status(400).json({ ok: false });
+	const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
+	// Create a token associated with the user
+	// If the user doesn't exist, it will be created using the connectOrCreate function
 	const token = await client.token.create({
 		data: {
-			payload: "1234",
+			payload,
 			user: {
-				connectOrCreate: { where: { ...payload }, create: { ...payload } },
+				connectOrCreate: {
+					where: { ...loginMethod },
+					create: { ...loginMethod },
+				},
 			},
 		},
 	});
-	console.log(token);
-	return res.status(200).end();
+	return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
