30c1e88d650da1f1237fd65dedd23795a376a289
seonhop <78877741+seonhop@users.noreply.github.com>
2023-07-15 23:12:49 -0400

    [Feature Update / Token Validation] Improved Token Verification and Session Management

--- pages/api/users/confirm.ts
+++ pages/api/users/confirm.ts
@@ -1,6 +1,7 @@
 import client from "@/pages/libs/server/client";
 import withHandler, { ResponseType } from "@/pages/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
 	req: NextApiRequest,
@@ -9,8 +10,25 @@ async function handler(
 	// Destructure phone and email from the request body
 	const { token } = req.body;
 	console.log(token);
+	const exists = await client.token.findUnique({
+		where: {
+			payload: token,
+		},
+		include: {
+			user: true,
+		},
+	});
+	if (!exists) res.status(404).end();
+	req.session.user = {
+		id: exists?.userId,
+	};
+	console.log(exists);
 
 	return res.json({ ok: true });
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+	cookieName: "session",
+	password:
+		"123183024128309183081203091380123801230129381083102831023812098312089034",
+});
